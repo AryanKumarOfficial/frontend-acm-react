4c62dcf4df29716defb4db7f69430f2f79772320
Aryan Kumar <aryanak9163@gmail.com>
2024-12-14 12:30:23 +0530

    adds pages to routing

--- src/router.jsx
+++ src/router.jsx
@@ -1,36 +1,71 @@
-import { createBrowserRouter } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
-import Privacy from "./pages/privacy.jsx";
-import Terms from "./pages/Terms.jsx";
+import Events from "@/pages/events.jsx";
+import Event from "@/pages/event.jsx";
+import Login from "@/pages/login.jsx";
+import EventDashboard from "@/pages/manage.jsx";
+import Projects from "@/pages/projects.jsx";
+import Project from "@/pages/project.jsx";
+import Register from "@/pages/register.jsx";
+import Team from "@/pages/team.jsx";
+import EventsPage from "@/pages/test.jsx";
 
 export const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/terms",
-        element: <Terms />,
-      },
-      {
-        path: "/privacy",
-        element: <Privacy />,
-      },
-    ],
-  },
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                path: "",
+                element: <Home/>,
+            },
+            {
+                path: "/about",
+                element: <About/>,
+            },
+            {
+                path: "/contact",
+                element: <Contact/>,
+            },
+            {
+                path: "/events",
+                element: <Events/>,
+            },
+            {
+                path: "/events/:slug",
+                element: <Event/>,
+            },
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "/manage",
+                element: <EventDashboard/>,
+            },
+            {
+                path: "/projects",
+                element: <Projects/>
+            },
+            {
+                path: "/projects/:slug",
+                element: <Project/>
+            },
+            {
+                path: "/register",
+                element: <Register/>
+            },
+            {
+                path: "/team",
+                element: <Team/>
+            },
+            {
+                path: "/test",
+                element: <EventsPage/>
+            }
+        ],
+    },
 ]);
